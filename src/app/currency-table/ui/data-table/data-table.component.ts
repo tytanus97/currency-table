e13166a24b0f527c3732900ffdcdf5d15c904ab8
pawel.ataman <pawel.ataman@finanteq.com>
2023-01-21 12:50:38 +0100

    fetch data

--- src/app/currency-table/ui/data-table/data-table.component.ts
+++ src/app/currency-table/ui/data-table/data-table.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { CurrenciesHttpClientService } from '../../communication/currencies-http-client.service'
 import { Currency, CurrencyTableData } from '../../domain/models'
+import { Table } from 'primeng/table'
 
 @Component({
 	selector: 'app-data-table',
@@ -10,16 +11,42 @@ import { Currency, CurrencyTableData } from '../../domain/models'
 export class DataTableComponent implements OnInit {
 
 	currencies: Currency[]
+	errorMessage = ''
 	constructor(private readonly currenciesHttpClientService: CurrenciesHttpClientService) { }
 
 	ngOnInit(): void {
+		this.fetchCurrencies()
 
-		this.currenciesHttpClientService.getCurrencyData().subscribe((data: CurrencyTableData) => {
-			console.log('data', data)
-			console.log('data', data.rates)
-
-			this.currencies = data.rates
-		})
 	}
 
+	onClearFilters(table: Table) {
+		if (table) {
+			table.reset()
+		}
+	}
+
+	onDateChanged(date: Date) {
+		this.fetchCurrencies(date)
+	}
+
+	private async fetchCurrencies(date?: Date): Promise<void> {
+		try {
+			const dataTable = date
+				? await this.currenciesHttpClientService.getCurrencyDataByDate(date)
+				: await this.currenciesHttpClientService.getCurrencyData()
+			if (!dataTable || !dataTable.rates.length) {
+				this.errorMessage = 'Brak danych'
+				return
+			}
+			this.currencies = dataTable.rates
+			this.errorMessage = ''
+		} catch (e) {
+			this.errorMessage = 'Nie udalo sie pobrac danych'
+		}
+
+	}
+
+	refreshData() {
+		this.fetchCurrencies()
+	}
 }
